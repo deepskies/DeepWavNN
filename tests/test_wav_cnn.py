7914f15ee34d14ecf5b57f607de122204c004569
voetberg <magpie127@gmail.com>
2023-03-10 14:06:00 -0600

    Corrected tests with stride included

--- tests/test_wav_cnn.py
+++ tests/test_wav_cnn.py
@@ -8,24 +8,144 @@ def test_input_layer():
     in_channels = 28
     out_channels = 1
     level = 1
-    hidden_size = 4
+    kernel_size = 4
+    stride_distance = 2
+    tail = True
+    network = WavCNN(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        level=level,
+        kernel_size=kernel_size,
+        stride_distance=stride_distance,
+        tail=tail,
+    )
+    assert (
+        network(torch.rand(*(1, in_channels, in_channels))).data.shape[1]
+        == out_channels
+    )
 
 
 def test_multibatch():
-    pass
+    in_channels = 28
+    out_channels = 1
+    level = 1
+    kernel_size = 4
+    stride_distance = 2
+    tail = True
+    network = WavCNN(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        level=level,
+        kernel_size=kernel_size,
+        stride_distance=stride_distance,
+        tail=tail,
+    )
+    assert (
+        network(torch.rand(*(64, in_channels, in_channels))).data.shape[1]
+        == out_channels
+    )
 
 
 def test_output_no_tail():
-    pass
 
-
-def test_output_yes_tail():
-    pass
+    in_channels = 28
+    out_channels = 1
+    level = 1
+    kernel_size = 4
+    stride_distance = 2
+    tail = True
+    network = WavCNN(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        level=level,
+        kernel_size=kernel_size,
+        stride_distance=stride_distance,
+        tail=tail,
+    )
+    assert (
+        network(torch.rand(*(1, in_channels, in_channels))).data.shape[1]
+        == out_channels
+    )
 
 
 def test_allowed_input_levels():
-    pass
+    kernel_sizes = []
+    allowed_levels = [1, 2, 3, 4]
+    for level, kernel_size in (allowed_levels, kernel_sizes):
+        in_channels = 28
+        out_channels = 1
+        stride_distance = 2
+        tail = True
+        network = WavCNN(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            level=level,
+            kernel_size=kernel_size,
+            stride_distance=stride_distance,
+            tail=tail,
+        )
+        assert (
+            network(torch.rand(*(1, in_channels, in_channels))).data.shape[1]
+            == out_channels
+        )
 
 
 def test_allowed_strides():
-    pass
+    stride_sizes = []
+    allowed_levels = [1, 2, 3, 4]
+    for level, kernel_size in (allowed_levels, stride_sizes):
+        in_channels = 28
+        out_channels = 1
+        kernel_size = 4
+        stride_distance = 2
+        tail = True
+        network = WavCNN(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            level=level,
+            kernel_size=kernel_size,
+            stride_distance=stride_distance,
+            tail=tail,
+        )
+        assert (
+            network(torch.rand(*(1, in_channels, in_channels))).data.shape[1]
+            == out_channels
+        )
+
+
+def test_forbidden_input_combos():
+    kernel_sizes = [45969, 4304, 2312, 12]
+    allowed_levels = [1, 2, 3, 4]
+    for level, kernel_size in (allowed_levels, kernel_sizes):
+        in_channels = 28
+        out_channels = 1
+        stride_distance = 2
+        tail = True
+        with pytest.raises(AssertionError):
+            WavCNN(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                level=level,
+                kernel_size=kernel_size,
+                stride_distance=stride_distance,
+                tail=tail,
+            )
+
+
+def test_forbidden_strides():
+    stride_sizes = [45969, 4304, 2312, 12]
+    allowed_levels = [1, 2, 3, 4]
+    for level, stride_distance in (allowed_levels, stride_sizes):
+        in_channels = 28
+        out_channels = 1
+        kernel_size = 4
+        tail = True
+        with pytest.raises(AssertionError):
+            WavCNN(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                level=level,
+                kernel_size=kernel_size,
+                stride_distance=stride_distance,
+                tail=tail,
+            )
