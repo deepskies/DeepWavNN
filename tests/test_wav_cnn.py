2edb91a574b993892101e2b0d407501389552e89
voetberg <magpie127@gmail.com>
2023-03-10 09:18:09 -0600

    Empty wavCNN class

---
+++ tests/test_wav_cnn.py
@@ -0,0 +1,31 @@
+import pytest
+import numpy as np
+import torch
+from wavNN.models.wavCNN import WavCNN
+
+
+def test_input_layer():
+    in_channels = 28
+    out_channels = 1
+    level = 1
+    hidden_size = 4
+
+
+def test_multibatch():
+    pass
+
+
+def test_output_no_tail():
+    pass
+
+
+def test_output_yes_tail():
+    pass
+
+
+def test_allowed_input_levels():
+    pass
+
+
+def test_allowed_strides():
+    pass
---
+++ wavNN/models/wavCNN.py
@@ -0,0 +1,10 @@
+import torch
+import pywt
+
+
+class WavCNN(torch.nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
