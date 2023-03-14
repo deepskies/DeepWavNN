2a612ad368800ca9ed8026b2d8947c2abe6937cb
voetberg <magpie127@gmail.com>
2023-03-14 15:51:48 -0500

    progress on cnn

--- wavNN/models/wavCNN.py
+++ wavNN/models/wavCNN.py
@@ -21,26 +21,34 @@ class WavCNN(torch.nn.Module):
             (wav_in - kernel)/stride+1
         ]
         """
-
+        super().__init__()
         self.level = level
         assert level != 0
 
-        in_layer_size = levels.find_output_size(level, in_channels)
+        in_layer_size = levels.find_output_size(level - 1, in_channels)
         self.wavelet = lambda x: torch.Tensor(pywt.wavedec2(x, "db1")[level])
 
-        conv_out_channels = (in_channels - kernel_size) / stride_distance
+        assert (
+            kernel_size < in_layer_size
+        ), f"Decrease the size of your kernel, kernel size {kernel_size}> input size {in_layer_size}"
+        assert (
+            stride_distance < in_layer_size
+        ), f"Decrease the size of your stride, greater than input size {in_layer_size}"
 
-        self.conv = torch.nn.Conv3d(
-            in_layer_size,
+        conv_out_channels = int(((in_channels - kernel_size) / stride_distance) + 1)
+
+        self.conv = torch.nn.Conv2d(
+            in_channels=in_layer_size,
             out_channels=conv_out_channels,
             kernel_size=kernel_size,
             stride=stride_distance,
         )
         self.relu = torch.nn.ReLU()
 
+        pool_kernel_size = int(conv_out_channels / 1.5)
+        pool_stride = int(pool_kernel_size)
+
         if pool:
-            pool_kernel_size = 0
-            pool_stride = 0
             self.pool = torch.nn.MaxPool3d(
                 kernel_size=pool_kernel_size, stride=pool_stride
             )
@@ -50,24 +58,28 @@ class WavCNN(torch.nn.Module):
 
             self.tail_flatten = torch.nn.Flatten()
             if not pool:
-                linear_in_size = conv_out_channels**3
+                linear_in_size = conv_out_channels * 3
             else:
-                linear_in_size = (
-                    (conv_out_channels - pool_kernel_size) / pool_stride
-                ) ** 3
+                linear_in_size = int(
+                    ((conv_out_channels - pool_kernel_size) / pool_stride) ** 3
+                )
 
             self.tail = torch.nn.Linear(linear_in_size, out_channels)
             self.tail_activation = torch.nn.Softmax()
 
     def forward(self, x):
-        wavelet = self.wavelet(x)
-        x = self.conv(wavelet)
+        x = self.wavelet(x)
+        x = torch.movedim(x, 0, -1)
+        print(x.shape)
+        print(self.conv)
+        x = self.conv(x)
         x = self.relu(x)
 
         if hasattr(self, "pool"):
             x = self.pool(x)
 
         if hasattr(self, "tail"):
+            print(x.shape)
             x = self.tail_flatten(x)
             x = self.tail(x)
             x = self.tail_activation(x)
