cf576f34630cf0ce7492012967204269399a63dd
voetberg <magpie127@gmail.com>
2023-03-17 08:14:46 -0500

    Adding kymatio implimentation

--- wavNN/models/wavelet_layer.py
+++ wavNN/models/wavelet_layer.py
@@ -1,18 +1,30 @@
+from typing import Union
 import torch
 import pywt
-import kymatio
+from kymatio.torch import Scattering2D
 
 
 class WaveletLayer:
-    def __init__(self, level: int, backend: str = "pywt") -> None:
+    def __init__(
+        self, level: int, input_size: Union[int, None] = None, backend: str = "pywt"
+    ) -> None:
 
         layers = {
             "pywt": lambda x: torch.Tensor(pywt.wavedec2(x, "db1")[level]),
-            "kymatio": NotImplemented,
+            "kymatio": lambda x: self.kymatio_layer(level=level, input_size=input_size)(
+                x
+            )[level],
         }
 
         assert backend in layers.keys()
         self.layer = layers[backend]
+
+    def kymatio_layer(self, level, input_size):
+        # Ref: This code
+        # https://www.kymat.io/gallery_2d/plot_invert_scattering_torch.html#sphx-glr-gallery-2d-plot-invert-scattering-torch-py
+
+        scattering = Scattering2D(J=2, shape=(input_size, input_size), max_order=level)
+        return scattering
 
     def __call__(self, x):
         return self.layer(x)
