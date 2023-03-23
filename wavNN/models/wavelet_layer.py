a0bcfab543f94467cb1ad09e132c558acdf26e98
voetberg <magpie127@gmail.com>
2023-03-23 14:01:12 -0500

    Single level 3 linear split network moved into the wavelet_layer file

--- wavNN/models/wavelet_layer.py
+++ wavNN/models/wavelet_layer.py
@@ -2,6 +2,7 @@ from typing import Union
 import torch
 import pywt
 from kymatio.torch import Scattering2D
+from wavNN.utils.levels import Levels
 
 
 class WaveletLayer:
@@ -28,3 +29,26 @@ class WaveletLayer:
 
     def __call__(self, x):
         return self.layer(x)
+
+
+class MiniWave(torch.nn.Module):
+    def __init__(self, level, in_channels, hidden_size) -> None:
+        super().__init__()
+        self.wavelet = WaveletLayer(level=level)
+        wav_in_channels = Levels.find_output_size(level, in_channels)
+
+        self.flatten_wavelet = torch.nn.Flatten(start_dim=1, end_dim=-1)
+        # Channels for each of the 3 channels of the wavelet (Not including the downscaled original
+        self.channel_1_mlp = torch.nn.Linear(wav_in_channels**2, hidden_size)
+        self.channel_2_mlp = torch.nn.Linear(wav_in_channels**2, hidden_size)
+        self.channel_3_mlp = torch.nn.Linear(wav_in_channels**2, hidden_size)
+
+    def forward(self, x):
+        x = self.wavelet(x)
+        # An MLP for each of the transformed levels
+        channel_1 = self.channel_1_mlp(self.flatten_wavelet(x[0]))
+        channel_2 = self.channel_2_mlp(self.flatten_wavelet(x[1]))
+        channel_3 = self.channel_3_mlp(self.flatten_wavelet(x[2]))
+        # stack the outputs
+        concat = torch.stack([channel_1, channel_2, channel_3], dim=1)
+        return concat
