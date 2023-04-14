be6b1263cd7de22e758d1a900e9eec7ccdf268e1
voetberg <magpie127@gmail.com>
2023-04-14 15:14:51 -0500

    greyscale

--- wavNN/data_generators/cifar_generator.py
+++ wavNN/data_generators/cifar_generator.py
@@ -1,11 +1,18 @@
 from wavNN.data_generators.data_generator import DataGenerator
-from torchvision.transforms import ToTensor
+from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
 
 class CIFARGenerator(DataGenerator):
     def __init__(self):
+        grayscale_transforms = transforms.Compose(
+            [transforms.Grayscale(num_output_channels=1), transforms.ToTensor()]
+        )
+
         dataset = CIFAR10(
-            root="wavNN/data/cifar10", download=True, train=True, transform=ToTensor()
+            root="wavNN/data/cifar10",
+            download=True,
+            train=True,
+            transform=grayscale_transforms,
         )
         super().__init__(dataset=dataset)
