acccfe7bac74a2012a0bd8fb999de61354cc399d
voetberg <magpie127@gmail.com>
2023-04-21 10:25:32 -0500

    Updated plots

--- wavNN/training/plot_results.py
+++ wavNN/training/plot_results.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
 
 from wavNN.training.training_metrics import TrainingMetrics
 
@@ -59,3 +59,112 @@ def plot_history(history, extra_metric_names, save_path=None):
     else:
         plt.show()
     plt.close("all")
+
+
+def plot_history_errorbar(
+    subplots,
+    histories: list,
+    extra_metrics,
+    save_path=None,
+    title="",
+    show=False,
+    label="",
+    color=None,
+    clear=False,
+):
+
+    for metric_index, metrics in enumerate(extra_metrics):
+
+        training = [history[f"train_{metrics.__name__}"] for history in histories]
+        val = [history[f"val_{metrics.__name__}"] for history in histories]
+
+        mean_training = pd.DataFrame(training).mean(axis=0)
+        std_training = pd.DataFrame(training).std(axis=0)
+
+        epochs = range(len(mean_training))
+
+        mean_val = pd.DataFrame(val).mean(axis=0)
+
+        subplots[metric_index].grid(
+            color="grey", linestyle="--", linewidth=0.5, alpha=0.6
+        )
+
+        subplots[metric_index].plot(epochs, mean_training, label=label, color=color)
+        subplots[metric_index].fill_between(
+            epochs,
+            mean_training - std_training,
+            mean_training + std_training,
+            alpha=0.3,
+            color=color,
+        )
+        subplots[metric_index].plot(epochs, mean_val, linestyle="dotted", color=color)
+        subplots[metric_index].set_ylabel(metrics.__name__)
+
+    training = [history[f"train_loss"] for history in histories]
+    val = [history[f"val_loss"] for history in histories]
+
+    mean_training = pd.DataFrame(training).mean(axis=0)
+    std_training = pd.DataFrame(training).std(axis=0)
+    mean_val = pd.DataFrame(val).mean(axis=0)
+
+    epochs = range(len(mean_training))
+
+    metric_index = -1
+
+    subplots[metric_index].plot(epochs, mean_training, label=label, color=color)
+    subplots[metric_index].fill_between(
+        epochs,
+        mean_training - std_training,
+        mean_training + std_training,
+        alpha=0.3,
+        color=color,
+    )
+    subplots[metric_index].plot(epochs, mean_val, linestyle="dotted", color=color)
+    subplots[metric_index].set_ylabel("Loss")
+    subplots[metric_index].grid(color="grey", linestyle="--", linewidth=0.5, alpha=0.6)
+
+    subplots[0].set_title(title.strip("_"))
+
+    if show:
+        plt.legend()
+        plt.show()
+
+    if clear:
+        plt.close("all")
+
+    if save_path is not None:
+        plt.savefig(f"{save_path}/history_errorbar.png")
+
+
+def plot_model_parameter_comparison(
+    num_params, inference_time, training_time, title, labels, colors
+):
+
+    fig, subplots = plt.subplots(nrows=3, ncols=1, figsize=(8, 10))
+
+    bar_x = [i + 1 for i in range(len(num_params))]
+    widths = [0.75 for _ in range(len(num_params))]
+
+    subplots[0].bar(bar_x, width=widths, height=num_params, color=colors)
+    subplots[0].set_title("Number Parameters")
+
+    inference_time = [np.asarray(time).mean() for time in inference_time]
+    inference_errorbar = [np.asarray(time).std() for time in inference_time]
+
+    subplots[1].bar(bar_x, width=widths, height=inference_time, color=colors)
+    subplots[1].errorbar(bar_x, widths, yerr=inference_errorbar)
+    subplots[1].set_title("Single Inference Time (s)")
+
+    train_time = [np.asarray(time).mean() for time in training_time]
+    train_errorbar = [np.asarray(time).std() for time in training_time]
+
+    bar = subplots[2].bar(bar_x, width=widths, height=train_time, color=colors)
+    subplots[1].errorbar(bar_x, widths, yerr=train_errorbar)
+    subplots[2].set_title("Full Training Time (s)")
+
+    subplots[2].bar_label(bar, label_position="center", labels=labels)
+
+    fig.legend()
+
+    plt.title(title)
+    plt.show()
